3f92b8dd247d45953763af6f5eef1f55ba54b4db
Finn Glück <finn.glueck@uni-konstanz.de>
2024-10-19 20:23:12 +0200

    Created new Handler

---
+++ src/Contexts/icon.java
@@ -0,0 +1,25 @@
+package Contexts;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class icon implements HttpHandler {
+    private byte[] bytes;
+    public icon() throws IOException {
+        InputStream reader = new FileInputStream("img/icon.png");
+        while (reader.available() > 0) {
+            bytes = reader.readAllBytes();
+        }
+        reader.close();
+    }
+    public void handle(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(200, bytes.length);
+        OutputStream stream = exchange.getResponseBody();
+        stream.write(bytes);
+        stream.close();
+    }
+}
